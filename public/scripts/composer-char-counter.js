5db3547495807c008800143795ee7252fed75352
Phil <52183389+Iconoclast-70@users.noreply.github.com>
2021-06-21 12:46:46 -0600

    Update composer-char-counter.js
    Updated major requirements for re-submission

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   console.log("ready");
+  let negativeCount;
+  let charCount;
   $( "#tweet-text" ).keyup(function() {
-    //$("#error-message").html(""); //Clear the error message div
-    //$('#current-count').html("140"); 
+    $("#error-message").html(""); //Clear the error message div
     const chars = $(this).val(); //Capture the characters from the text area
     //Update the counter, if the character count is over 140, change the colour to red and count backwards
     if (chars.length === 141) {
@@ -11,8 +12,9 @@ $(document).ready(function() {
     }
     //Capture each character entered and increment the letter count for the tweet
     if (chars.length < 141) {
-      $('#current-count').html(String(chars.length));    
+      charCount = Number($('#current-count').html());
       $('#current-count').css('color','black');
+      $('#current-count').html(String(charCount - 1));  
     } else if (chars.length > 141) {
       negativeCount = Number($('#current-count').html());
       $('#current-count').css('color','red');
@@ -20,8 +22,4 @@ $(document).ready(function() {
     }
   });
 });
-
-
-
-
 
